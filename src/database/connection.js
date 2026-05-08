fba4aea2a7036e769f89fb3487c08a91cf9431c6
Rin Sarik <sarik91688@gmail.com>
2026-05-09 00:42:49 +0700

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,11 @@
 const { Sequelize } = require("sequelize");
 const { logger } = require("sequelize/lib/utils/logger");
 
-const Connection = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false
-  },
+const sequelize = new Sequelize(
+  
+mysql://root:MezIDowwBvdcyzvEcuxgAnPIfOQbJkVH@mysql.railway.internal:3306/railway,{
+  dialect: 'mysql'
+}
 );
 
 module.exports = Connection;
